d9148e2bcffa7b8237b52d1c6df7a15d0c08f94e
Chamith <chamith@sp-solutions.biz>
2016-10-31 16:42:44 +0530

    -added dependencies to pom xml -added hibernate configuration -added log4j configuration

--- src/main/java/com/vgates/App.java
+++ src/main/java/com/vgates/App.java
@@ -4,7 +4,7 @@ package com.vgates;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
---
+++ src/main/java/com/vgates/session/HibernateSessionManager.java
@@ -0,0 +1,36 @@
+package com.vgates.session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by Chamith on 10/31/2016.
+ */
+
+
+public class HibernateSessionManager {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+
+}
+
