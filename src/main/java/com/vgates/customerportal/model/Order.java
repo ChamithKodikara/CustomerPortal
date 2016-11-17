59488c5b355f9cbfe09178c79c1783d25a671e43
Chamith <chamith@sp-solutions.biz>
2016-11-17 17:28:26 +0530

    -added payment & Order model Classes

---
+++ src/main/java/com/vgates/customerportal/model/Order.java
@@ -0,0 +1,107 @@
+package com.vgates.customerportal.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Chamith on 11/15/2016.
+ */
+@Entity
+@Table(name = "ORDER")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "ACTIVE")
+    private Boolean active = true;
+
+    @Column(name = "ORDER_AMOUNT")
+    private Double orderAmount;
+
+    @Column(name = "ORDER_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Column(name = "ORDER_NO")
+    private String orderNo;
+
+    @Column(name = "COMMENT")
+    private String comment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Double getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Double orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Order)) {
+            return false;
+        }
+        Order other = (Order) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.vgates.customerportal.model.Order[ id=" + id + " ]";
+    }
+}
